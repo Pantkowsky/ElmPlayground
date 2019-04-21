89d44d51e2f5d60275bff0594d6984cf02e20b3a
Pantkowsky <a.pantkowski@gmail.com>
2019-04-21 22:50:29 +0200

    Added `Constants` file for storing project constants

---
+++ src/Constants.elm
@@ -0,0 +1,16 @@
+module Constants exposing (..)
+
+wrongPasswordMessage: String
+wrongPasswordMessage = "Incorrect password. Have you re-typed it correctly?"
+
+shortPasswordMessage: String
+shortPasswordMessage = "Password is too short. Use more than 8 characters"
+
+successMessage: String
+successMessage = "Sign-up successful"
+
+incorrectAgeMessage: String
+incorrectAgeMessage = "You need to be 18 to sign-up"
+
+requiredLength: Int
+requiredLength = 8
